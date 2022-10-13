a379913e3bf3f66f342b1415df0bc8de837e0629
Ira114822 <odaa1976.com@gmail.com>
2022-10-13 21:29:30 +0300

    123

--- task03.py
+++ task03.py
@@ -1,2 +1,17 @@
 # 5! = 5 * 4 * 3 * 2 * 1
-f(5!)
+# f(5!)
+
+def factorial(number):
+    if number <=1:
+        return 1
+
+    return number * factorial(number - 1)
+
+def main():
+    number = int(input("Input number: "))
+    fac = factorial(number)
+
+    msg = f"Result = {fac}"
+    print(msg)
+
+main()
